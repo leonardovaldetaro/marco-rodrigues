6eb6faf51bd6f45f9718d7c0eb52a0e2a6dde322
Leonardo Valdetaro <leonardovaldetaro@gmail.com>
2025-07-15 11:46:06 +0100

    mais um deploy para tentar corrigir o erro de tipagem

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,36 +1,26 @@
 // app/blog/[slug]/page.tsx
-import Styles from './Slug.module.scss';
-import { getPostBySlug, getAllPosts } from "../../lib/posts";
-import { notFound } from "next/navigation";
-import Link from 'next/link';
+import Styles from './Slug.module.scss'
+import { getPostBySlug, getAllPosts } from '../../lib/posts'
+import { notFound } from 'next/navigation'
+import Link from 'next/link'
 
 export async function generateStaticParams() {
-  const posts = getAllPosts();
-  return posts.map((post) => ({
-    slug: post.slug
-  }));
+  const posts = getAllPosts()
+  return posts.map((post) => ({ slug: post.slug }))
 }
 
-export async function generateMetadata({ params }: BlogPostPageProps) {
-  const post = await getPostBySlug(params.slug);
-  if (!post) return {};
-
+export async function generateMetadata({ params }) {
+  const post = await getPostBySlug(params.slug)
+  if (!post) return {}
   return {
     title: post.title,
     description: post.excerpt,
-  };
+  }
 }
 
-interface BlogPostPageProps {
-  params: {
-    slug: string;
-  };
-}
-
-export default async function BlogPostPage({ params }: BlogPostPageProps) {
-  const post = await getPostBySlug(params.slug);
-
-  if (!post) return notFound();
+export default async function BlogPostPage({ params }) {
+  const post = await getPostBySlug(params.slug)
+  if (!post) return notFound()
 
   return (
     <section className={Styles.blogPostPage}>
@@ -40,8 +30,10 @@ export default async function BlogPostPage({ params }: BlogPostPageProps) {
       <h1 className={Styles.blogPostPage__title}>{post.title}</h1>
       <p className={Styles.blogPostPage__date}>{post.date}</p>
       <article className={Styles.blogPostPage__contenContainer}>
-        <p className={Styles.blogPostPage__contenContainer__content}>{post.content}</p>
+        <p className={Styles.blogPostPage__contenContainer__content}>
+          {post.content}
+        </p>
       </article>
     </section>
-  );
+  )
 }
